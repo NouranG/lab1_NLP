3d7046a6f18de5983d60ff0b3a9a3c072424c3f5
NouranG <nouranlahzy38@gmail.com>
2026-05-28 09:06:13 +0300

    editing bm25 script

--- src/vectorization/bm25.py
+++ src/vectorization/bm25.py
@@ -1,5 +1,6 @@
 from rank_bm25 import BM25Okapi
 
+
 class BM25Vectorizer:
     def __init__(self, text_processor):
         self._text_processor = text_processor
@@ -7,12 +8,25 @@ class BM25Vectorizer:
         self._corpus = None
 
     def fit(self, texts):
-        tokenized_corpus = [self._text_processor(text) for text in texts]
+
+        tokenized_corpus = [
+            self._text_processor.process(text).split()
+            for text in texts
+        ]
+
         self._corpus = tokenized_corpus
         self._bm25 = BM25Okapi(tokenized_corpus)
 
-    def search(self,query,top_k=5):
-        tokenized_query = self._text_processor(query)
+        return self
+
+    def search(self, query, top_k=5):
+
+        tokenized_query = (
+            self._text_processor.process(query).split()
+        )
+
         scores = self._bm25.get_scores(tokenized_query)
+
         top_k_indices = scores.argsort()[-top_k:][::-1]
+
         return top_k_indices, scores[top_k_indices]
