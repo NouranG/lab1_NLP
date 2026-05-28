50f301e4d2b06a86efae86d2b6cc60127ae59120
NouranG <nouranlahzy38@gmail.com>
2026-05-28 07:35:37 +0300

    editing bm25 vectors

--- src/vectorization/bm25.py
+++ src/vectorization/bm25.py
@@ -1,18 +1,18 @@
 from rank_bm25 import BM25Okapi
 
 class BM25Vectorizer:
-    def __init__(self, preprocessor):
-        self._preprocessor = preprocessor
+    def __init__(self, text_processor):
+        self._text_processor = text_processor
         self._bm25 = None
         self._corpus = None
 
     def fit(self, texts):
-        tokenized_corpus = [text.split() for text in texts]
+        tokenized_corpus = [self._text_processor(text) for text in texts]
         self._corpus = tokenized_corpus
         self._bm25 = BM25Okapi(tokenized_corpus)
 
     def search(self,query,top_k=5):
-        tokenized_query = query.split()
+        tokenized_query = self._text_processor(query)
         scores = self._bm25.get_scores(tokenized_query)
         top_k_indices = scores.argsort()[-top_k:][::-1]
         return top_k_indices, scores[top_k_indices]
--- src/vectorization/tfidf.py
+++ src/vectorization/tfidf.py
@@ -1,10 +1,10 @@
-from sklearn.feature_extraction.text import tfidf_vectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class TFIDFVectorizer:
     """TF-IDF vectorizer, using sklearn's TfidfVectorizer."""
     def __init__(self, text_processor,max_features=5000,ngram_range=(1, 1)):
         self._text_processor = text_processor
-        self._vectorizer = tfidf_vectorizer(max_features=max_features, ngram_range=ngram_range)
+        self._vectorizer = TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)
 
     def fit_transform(self,texts):
         return self._vectorizer.fit_transform(texts)
